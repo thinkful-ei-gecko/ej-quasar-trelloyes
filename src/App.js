6a2a83c80c8b838f6e9bc5c6030960cc26fa648a
EJ <ejg132@gmail.com>
2019-09-03 11:59:03 -0700

    css not applying

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Card from './Card.js';
 import List from './List.js';
+import STORE from './store.js';
+import './App.css';
 
-function App(prop) {
-  return (
-    <main className='App'>
-      <header className="App-header">
-        <h1>Trelloyes!</h1>
-      </header>
-      <div className="App-List">
-        <List />
-      </div>
-      {/* content goes here */}
-    </main>
-  );
-}
+
+class App extends Component {
+
+  render(){
+      return (
+        <main className='App'>
+          <header className="App-header">
+            <h1>Trelloyes!</h1>
+          </header>
+          <div className="App-List">
+            {STORE.lists.map(list => (
+              <List 
+                key={list.id}
+                header={list.header}
+                cards={list.cardIds.map(id => STORE.allCards[id])}
+                />))}
+          </div>
+        </main>
+      );
+    }
+  }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Card from './Card.js';
+import './List.css';
 
-function List(prop) {
+function List(props) {
+
   return (
     <section className="List">
       <header className="List-header">
-        <h2>{prop.header}</h2>
+        <h2>{props.header}</h2>
       </header>
       <div className="List-cards">
-        {
-          
-        }
+        {props.cards.map((card) =>
+          <Card 
+          key={card.id}
+          title={card.title}
+          content={card.content}
+          />
+        )}
       </div>
+      
     </section>
   );
 
